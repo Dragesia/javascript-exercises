120ad458ee5fe0f404e17242eb3b1297da5c8373
Dragesia <brkgzlsy@gmail.com>
2022-06-24 01:14:07 +0300

    First 7 exercises done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    const myArray = str.split("");
+    const newArray = [];
 
+    for(let i=0; i<str.length; i++) {
+        newArray[i] = myArray[str.length-1-i];
+    }
+
+    return newArray.join("");
 };
 
 // Do not edit below this line
