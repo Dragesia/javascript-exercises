e9381e33532beacd29a3264bf6e09bb86f9b514e
Dragesia <brkgzlsy@gmail.com>
2022-07-06 17:17:47 +0300

    done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  return parseInt(args.reduce((result, num) => {
+    result += num;
+    return result;
+  }, 0));
+
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  return parseInt(args.reduce((result, num) => {
+    result *= num;
+    return result;
+  }, 1));
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a == 0) return 1;
+	let result = 1;
+  for(let i=a; i>0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
