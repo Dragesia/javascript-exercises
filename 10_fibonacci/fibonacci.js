6ed237f702bf2440dd359fa81dfda8a86fc8403e
Dragesia <brkgzlsy@gmail.com>
2022-07-06 17:50:57 +0300

    done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if (a<0) return "OOPS";
+    let arr = [1, 1];
+    for(let i=2; i<=a; i++) {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    return arr[a-1];
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(ppl) {
+    let oldest = 0, index;
+    for (let i=0; i<ppl.length; i++) {
+        if(ppl[i].yearOfDeath == undefined) {
+            ppl[i].age = (new Date()).getFullYear() - ppl[i].yearOfBirth; 
+        } else {
+            ppl[i].age = ppl[i].yearOfDeath - ppl[i].yearOfBirth;
+        }
 
+        if(ppl[i].age > oldest) {
+            oldest = ppl[i].age;
+            index = i;
+        }
+    }
+
+    return ppl[index];
 };
 
 // Do not edit below this line
